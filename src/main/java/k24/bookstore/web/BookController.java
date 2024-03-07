e18e15f64451a68129713af30fd9f19e132a7911
Anni Seppänen <seppanenanni@hotmail.com>
2024-03-07 12:20:25 +0200

    Controller edited

--- src/main/java/k24/bookstore/web/BookController.java
+++ src/main/java/k24/bookstore/web/BookController.java
@@ -1,6 +1,5 @@
 package k24.bookstore.web;
-import java.util.List;
-import java.util.Optional;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -8,34 +7,42 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 import k24.bookstore.domain.Book;
 import k24.bookstore.domain.BookRepository;
 import k24.bookstore.domain.CategoryRepository;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-
 @Controller
 public class BookController {
     @Autowired
-    private BookRepository bookRepository;
-
+    BookRepository bookRepository;
     @Autowired
-    private CategoryRepository categoryRepository;
+    CategoryRepository categoryRepository;
 
-    @RequestMapping(value="/login")
-    public String login() {
+    @GetMapping("/login") 
+    public String loginPage() {
         return "login";
     }
-    
-    @RequestMapping("/booklist")
+
+    @GetMapping("/booklist")
     public String bookList(Model model) {
         model.addAttribute("books", bookRepository.findAll());
         return "booklist";
+    }
+
+    @PostMapping("/saveBook")
+    public String saveBook(Book book) {
+        bookRepository.save(book);
+        return "redirect:booklist";
+    }
+
+    @PreAuthorize("hasAuthority('ADMIN')") 
+    @GetMapping("delete/{id}") 
+    public String deleteBook(@PathVariable("id") Long id, Model model) {
+        bookRepository.deleteById(id);
+        return "redirect:../booklist";
     }
 
     @RequestMapping("/addBook")
@@ -45,19 +52,6 @@ public class BookController {
         return "addBook";
     }
 
-    @PostMapping("/saveBook")
-    public String saveBook(Book book) {
-        bookRepository.save(book);
-        return "redirect:booklist";
-    } 
-
-    @PreAuthorize("hasAuthority('ADMIN')") 
-    @GetMapping("delete/{id}") 
-    public String deleteBook(@PathVariable("id") Long id, Model model) {
-        bookRepository.deleteById(id);
-        return "redirect:../booklist";
-    }
-
     @GetMapping("editBook/{id}")
     public String editBook(@PathVariable("id") Long id, Model model) {
         model.addAttribute("book", bookRepository.findById(id));
@@ -65,23 +59,4 @@ public class BookController {
         return "editBook";
     }
 
-    @PutMapping("/book/{id}")
-    Book editBook(@RequestBody Book editedBook, @PathVariable Long id) {
-        editedBook.setId(id);
-        return bookRepository.save(editedBook);
-    }
-
-    @GetMapping("/books")
-    public @ResponseBody List<Book> bookListRest() {
-        return (List<Book>) bookRepository.findAll();
-    }
-
-    @GetMapping("/book/{id}")
-    public @ResponseBody Optional<Book> findBookRest(@PathVariable("id") Long id) {
-        return bookRepository.findById(id);
-    }
-    
-    
- }
-
-
+}
